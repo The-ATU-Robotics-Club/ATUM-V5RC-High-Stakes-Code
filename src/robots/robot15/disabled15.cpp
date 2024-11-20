5554dcaa84175d9b7c46c9ce8830224cf0fa5ff1
theguy20phi <46367025+theguy20phi@users.noreply.github.com>
2024-11-20 15:50:15 -0600

    Add default brain ID.

--- src/robots/robot15/disabled15.cpp
+++ src/robots/robot15/disabled15.cpp
@@ -1,7 +1,7 @@
 #include "robot15.hpp"
 
 namespace atum {
-Robot15::Robot15() : Robot{this} {}
+Robot15::Robot15() : Robot{this}, leftMotors{1, 2, 3} {}
 
 void Robot15::disabled() {}
 } // namespace atum
