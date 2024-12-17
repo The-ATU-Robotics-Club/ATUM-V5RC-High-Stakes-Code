83c522a090b71e3f65e5615ed850b2420adec3d5
theguy20phi <bradenwepierce@gmail.com>
2024-12-16 20:02:56 -0600

    Document and adjust odometry.

--- include/atum/position/odometry.hpp
+++ include/atum/position/odometry.hpp
@@ -7,18 +7,52 @@
 #include "tracker.hpp"
 
 namespace atum {
+/**
+ * @brief Performs tracking using two odometers perpendicular to eachother.
+ * Uses arc estimation and twists for improved accuracy.
+ *
+ */
 class Odometry : public Tracker, public Task {
-  TASK_BOILERPLATE();
+  TASK_BOILERPLATE(); // Included in all task derivatives for setup.
 
   public:
+  /**
+   * @brief Constructs a new Odometry object. Expects an odometer parallel to
+   * the direction of travel, an odometer perpendicular to the direction of
+   * travel, and an IMU.
+   *
+   * The startBackgroundTasks() method will have to be called if you expect
+   * tracking to be performed in the background.
+   *
+   * @param iForward
+   * @param iSide
+   * @param iImu
+   * @param loggerLevel
+   */
   Odometry(std::unique_ptr<Odometer> iForward,
            std::unique_ptr<Odometer> iSide,
            std::unique_ptr<IMU> iImu,
            Logger::Level loggerLevel = Logger::Level::Info);
 
+  /**
+   * @brief Updates the current position as tracked by the odometers.
+   * Typically called in the background, but could be manually called.
+   *
+   * @return Position
+   */
   Position update() override;
 
   private:
+  /**
+   * @brief Checks that dx, dy, and dh are finite, valid values before
+   * rotating them appropriately and adding them to the current position
+   * estimate.
+   *
+   * @param dx
+   * @param dy
+   * @param dh
+   * @return Position
+   */
   Position integratePosition(inch_t dx, inch_t dy, radian_t dh);
 
   std::unique_ptr<Odometer> forward;
--- src/atum/position/odometry.cpp
+++ src/atum/position/odometry.cpp
@@ -11,7 +11,7 @@ Odometry::Odometry(std::unique_ptr<Odometer> iForward,
     side{std::move(iSide)},
     imu{std::move(iImu)} {
   if(!forward) logger.error("The forward odometer must be provided.");
-  if(!side) logger.warn("The side odometer was not provided.");
+  if(!side) logger.error("The side odometer was not provided.");
   if(!imu) logger.error("An IMU must be provided.");
   logger.info("Odometry constructed!");
 }
@@ -21,44 +21,36 @@ Position Odometry::update() {
   inch_t dyR{forward->traveled()};
   const radian_t dh{imu->getTraveled()};
   const double dhScalar{getValueAs<radian_t>(dh)};
-  scalar_t sinDHOverDH;
-  scalar_t cosDHMinusOneOverDH;
+  // Initial values are found by their Taylor series evaluated at dh = 0.
+  scalar_t sinDHOverDH{1.0};
+  scalar_t cosDHMinusOneOverDH{0.0};
   if(dh) {
+    // Arc length estimation using the distance from the center to the
+    // odometers.
     dxR = 2.0 * sin(dh / 2.0) * (dxR / dhScalar + side->getFromCenter());
     dyR = 2.0 * sin(dh / 2.0) * (dyR / dhScalar + forward->getFromCenter());
     sinDHOverDH = sin(dh) / dhScalar;
     cosDHMinusOneOverDH = (cos(dh) - 1.0) / dhScalar;
-  } else {
-    sinDHOverDH = 1.0 - (dhScalar * dhScalar) / 6.0;
-    cosDHMinusOneOverDH = -dhScalar / 2.0;
   }
-  const inch_t dxG{sinDHOverDH * dxR + cosDHMinusOneOverDH * dyR};
-  const inch_t dyG{-cosDHMinusOneOverDH * dxR + sinDHOverDH * dyR};
-  /*
-  if(headingChange) {
-    sideChange = 2.0 * sin(headingChange / 2.0) *
-                 (sideChange / getValueAs<radian_t>(headingChange) +
-                  side->getFromCenter());
-    forwardChange = 2.0 * sin(headingChange / 2.0) *
-                    (forwardChange / getValueAs<radian_t>(headingChange) +
-                     forward->getFromCenter());
-  }*/
-  return integratePosition(dxG, dyG, dh);
+  // Accounting for angular velocity.
+  const inch_t dxRAdj{sinDHOverDH * dxR + cosDHMinusOneOverDH * dyR};
+  const inch_t dyRAdj{-cosDHMinusOneOverDH * dxR + sinDHOverDH * dyR};
+  return integratePosition(dxRAdj, dyRAdj, dh);
 }
 
 Position Odometry::integratePosition(inch_t dx, inch_t dy, radian_t dh) {
   if(!std::isfinite(getValueAs<inch_t>(dx)) ||
      !std::isfinite(getValueAs<inch_t>(dy)) ||
      !std::isfinite(getValueAs<radian_t>(dh))) {
-    logger.error("Invalid values read from odometers.");
+    logger.warn("Invalid values read from odometers.");
     dx = 0_in;
     dy = 0_in;
     dh = 0_rad;
   }
   Position currentPosition{getPosition()};
-  const radian_t averageHeading{currentPosition.h + 0.5 * dh};
-  currentPosition.x += cos(averageHeading) * dx + sin(averageHeading) * dy;
-  currentPosition.y += -sin(averageHeading) * dx + cos(averageHeading) * dy;
+  const radian_t h0{currentPosition.h};
+  currentPosition.x += cos(h0) * dx + sin(h0) * dy;
+  currentPosition.y += -sin(h0) * dx + cos(h0) * dy;
   currentPosition.h += dh;
   setPosition(currentPosition);
   return getPosition(); // Use getPosition() for logging purposes.
