51315f7ff275ca37b041a55805491da47ea09896
Optic20 <MREWO20@gmail.com>
2025-01-20 20:21:12 -0600

    Added controls for rush mech

--- src/robots/robotClone/autonomousClone.cpp
+++ src/robots/robotClone/autonomousClone.cpp
@@ -13,10 +13,56 @@ ROUTINE_DEFINITIONS_FOR(RobotClone) {
    |___/_\_\_|_|_/__/
   */
   START_ROUTINE("Skills")
+<<<<<<< Updated upstream
   // 24 in code
   setupRoutine({-5_ft, 0_ft, 90_deg});
   intake->intake();
   pathFollower->follow({{{-4_ft, 0_ft, 90_deg}, false, Path::Parameters{}}});
+=======
+  
+  END_ROUTINE
+
+  START_ROUTINE("Full Test")
+  setupRoutine({-5_ft, -3_ft, 180_deg});
+
+  pathFollower->follow(
+      {{{-1_ft, -4.625_ft, 90_deg}, false, Path::Parameters{2.8, 25_in_per_s}}},
+      "Test Curve");
+  turn->toward(-15_deg);
+  pathFollower->follow(
+      {{{0_tile, 0_tile, 45_deg}, false, Path::Parameters{3.5}}});
+  turn->awayFrom(135_deg);
+  pathFollower->follow(
+      {{{1.5_tile, -2.5_tile, 0_deg}, true, Path::Parameters{4}},
+       {{1.5_tile, -1.5_tile, 0_deg}, false}});
+  turn->toward({0_in, 0_in});
+
+  END_ROUTINE
+
+  START_ROUTINE("Turning Test")
+  setupRoutine({0_in, 0_in, 0_deg});
+
+  turn->toward(90_deg);    // Right
+  turn->toward(180_deg);   // Right
+  turn->toward(-90_deg);   // Right
+  turn->awayFrom(0_deg);   // Left
+  turn->awayFrom(180_deg); // About
+  turn->toward(90_deg);    // Right
+  turn->toward(-90_deg);   // About
+  turn->awayFrom(0_deg);   // Left
+  turn->awayFrom(180_deg); // About
+  turn->toward(45_deg);
+  turn->toward(90_deg);
+  turn->toward(135_deg);
+  turn->toward(180_deg);
+  turn->toward(225_deg);
+  turn->toward(270_deg);
+  turn->toward(315_deg);
+  turn->toward(360_deg);
+  END_ROUTINE
+
+  START_ROUTINE("Ladybrown Test")
+>>>>>>> Stashed changes
   intake->load();
   pathFollower->follow({{{-5.5_ft, 0_ft, 90_deg}, false, Path::Parameters{}}});
   // LADY BROWN
