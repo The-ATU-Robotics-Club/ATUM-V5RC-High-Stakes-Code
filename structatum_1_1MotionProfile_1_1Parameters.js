56a8d626081cb9884d48b25a3e5a8b6a198da53b
theguy20phi <46367025+theguy20phi@users.noreply.github.com>
2025-02-11 09:56:59 +0000

    Deploying to gh-pages from @ The-ATU-Robotics-Club/ATUM-V5RC-High-Stakes-Code@f77b7714713577c075d5d87cf4db7410c2cef9e4 🚀

--- structatum_1_1MotionProfile_1_1Parameters.js
+++ structatum_1_1MotionProfile_1_1Parameters.js
@@ -2,5 +2,6 @@ var structatum_1_1MotionProfile_1_1Parameters =
 [
     [ "Parameters", "structatum_1_1MotionProfile_1_1Parameters.html#a88a521b22e2696c18b18006ec69bc070", null ],
     [ "Parameters", "structatum_1_1MotionProfile_1_1Parameters.html#a3b970f0baf4ce5ac77cd35edf1f24ad1", null ],
-    [ "operator=", "structatum_1_1MotionProfile_1_1Parameters.html#a76c73645b2284dbd2dce9fff15096f0d", null ]
+    [ "reset", "structatum_1_1MotionProfile_1_1Parameters.html#a9be616998b2e5267b120b69b7fef6bbe", null ],
+    [ "setSpecial", "structatum_1_1MotionProfile_1_1Parameters.html#af1f35567038302dcc536bd829969c238", null ]
 ];
